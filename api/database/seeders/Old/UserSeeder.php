706c16f25f80ba260f9e081d4b86542b1c9c0d9a
Karl <karl@ethesia.com>
2023-01-30 02:36:07 +0800

    Update seeder

--- api/database/seeders/Old/UserSeeder.php
+++ api/database/seeders/Old/UserSeeder.php
@@ -22,198 +22,6 @@ class UserSeeder extends Seeder
         // php artisan db:seed --class="Database\\Seeders\\Old\\UserSeeder"
         
 
-    //     $users = DB::connection('mysql2')->select("
-    //     SELECT DISTINCT
-    //     propertytypes.`Name`, 
-    //     clients.SequenceNo, 
-    //     clients.LastName, 
-    //     clients.FirstName, 
-    //     clients.MiddleName, 
-    //     clients.Suffix, 
-    //     clients.BirthDate, 
-    //     clients.Gender, 
-    //     clients.CivilStatus, 
-    //     clients.Religion, 
-    //     clients.TIN, 
-    //     clients.ContactNumber, 
-    //     clients.ZipCode, 
-    //     clients.Address, 
-    //     clients.Nationality, 
-    //     clients.EducationalAttn, 
-    //     clients.NumberOfDependents, 
-    //     clients.School, 
-    //     clients.MonthlyIncome, 
-    //     clients.MonthlyHouseholdIncome, 
-    //     clients.SpouseLastName, 
-    //     clients.SpouseFirstName, 
-    //     clients.SpouseMiddleName, 
-    //     clients.SpouseBirthDate, 
-    //     clients.SpouseGender, 
-    //     clients.SpouseTIN, 
-    //     clients.SpouseNumber, 
-    //     clients.CoFirstName, 
-    //     clients.CoLastName, 
-    //     clients.CoMiddleName, 
-    //     clients.CoSuffix, 
-    //     clients.CoBirthDate, 
-    //     clients.CoTIN, 
-    //     clients.CoGender, 
-    //     clients.CoNumber, 
-    //     clients.AtLastName, 
-    //     clients.AtFirstName, 
-    //     clients.AtMiddleName, 
-    //     clients.AtSuffix, 
-    //     clients.AtBirthDate, 
-    //     clients.AtGender, 
-    //     clients.AtTIN, 
-    //     clients.AtNumber, 
-    //     clients.Employment, 
-    //     clients.EmploymentType, 
-    //     clients.CompanyName, 
-    //     clients.Industry, 
-    //     clients.CompanyLocation, 
-    //     clients.DateEmployed, 
-    //     clients.Profession, 
-    //     clients.Position, 
-    //     clients.HomeNumber, 
-    //     clients.OfficeNumber
-    // FROM
-    //     clients
-    //     INNER JOIN
-    //     transactions
-    //     ON 
-    //         clients.Id = transactions.ClientId
-    //     INNER JOIN
-    //     properties
-    //     ON 
-    //         transactions.PropertyId = properties.Id
-    //     INNER JOIN
-    //     propertytypes
-    //     ON 
-    //         properties.PropertyTypeId = propertytypes.Id
-    // WHERE
-    //     FirstName IS NOT NULL
-    //     ");
-
-    //     foreach ($users as $user) {
-    //         $name = ucwords($user->FirstName)  . ' ' . ucwords($user->MiddleName) . ' ' . ucwords($user->LastName);
-    //         $slug = SlugService::createSlug(User::class, 'slug', $name);
-    //         $date_of_birth = $user->BirthDate;
-
-    //         $user_data = User::updateOrCreate([
-    //             "role_id" => 5,
-    //             'branch_id' => 1,
-    //             'email' => $slug .'@gamil.com',
-    //             'slug' => $slug
-    //         ], [
-    //             'password' => Hash::make('secretpass' . strtolower($user->LastName)), // password
-    //         ]);
-
-    //         if (
-    //             $user->BirthDate == '0001-01-02 00:00:00.000000' || 
-    //             $user->BirthDate == '0001-01-02 07:52:58.000000' ||
-    //             $user->BirthDate == '0001-01-01 00:00:00.000000' ||
-    //             $user->BirthDate == '0001-01-01 15:56:00.000000' ||
-    //             $user->BirthDate == '0001-01-02 15:56:00.000000' 
-    //         ) {
-    //             $date_of_birth = null;
-    //         }
-
-    //         $user_data->profile()->withoutGlobalScope('default_branch')
-    //         ->updateOrCreate([
-    //             'first_name' => ucwords($user->FirstName),
-    //             'last_name' => ucwords($user->LastName),
-    //             'middle_name' => ucwords($user->MiddleName)
-    //         ], [
-    //             'suffix' => ucwords($user->Suffix),
-    //             'gender' => ucwords($user->Gender),
-    //             'tin' => ucwords($user->TIN),
-    //             'date_of_birth' => $date_of_birth,
-    //             'civil_status' => ucwords($user->CivilStatus),
-    //             'religion' => ucwords($user->Religion),
-    //             'contact_number' => ucwords($user->ContactNumber),
-    //             'zip_code' => ucwords($user->ZipCode),
-    //             'address' => ucwords($user->Address),
-    //             'photo' => null,
-    //         ]);
-
-    //         if ($user->SpouseFirstName && $user->SpouseLastName) {
-    //             $user_data->spouses()->withoutGlobalScope('default_branch')
-    //             ->updateOrCreate([
-    //                 'first_name' => ucwords($user->SpouseFirstName),
-    //                 'last_name' => ucwords($user->SpouseLastName)
-    //             ], [
-    //                 'middle_name' => ucwords($user->SpouseMiddleName),
-    //                 'gender' => ucwords($user->SpouseGender),
-    //                 'tin' => ucwords($user->SpouseTIN),
-    //                 'date_of_birth' => $user->SpouseBirthDate,
-    //                 'contact_number' => ucwords($user->SpouseNumber)
-    //             ]);
-    //         }
-            
-    //         if ($user->CoFirstName && $user->CoLastName) {
-    //             $user_data->coBorrowers()->withoutGlobalScope('default_branch')
-    //             ->updateOrCreate([
-    //                 'first_name' => ucwords($user->CoFirstName),
-    //                 'last_name' => ucwords($user->CoLastName)
-    //             ], [
-    //                 'middle_name' => ucwords($user->SpouseMiddleName),
-    //                 'suffix' => ucwords($user->CoSuffix),
-    //                 'gender' => ucwords($user->CoGender),
-    //                 'tin' => ucwords($user->CoTIN),
-    //                 'date_of_birth' => $user->CoBirthDate,
-    //                 'contact_number' => ucwords($user->CoNumber)
-    //             ]);
-    //         }
-
-    //         if ($user->CoFirstName && $user->CoLastName) {
-    //             $user_data->coBorrowers()->withoutGlobalScope('default_branch')
-    //             ->updateOrCreate([
-    //                 'first_name' => ucwords($user->CoFirstName),
-    //                 'last_name' => ucwords($user->CoLastName)
-    //             ], [
-    //                 'middle_name' => ucwords($user->SpouseMiddleName),
-    //                 'suffix' => ucwords($user->CoSuffix),
-    //                 'gender' => ucwords($user->CoGender),
-    //                 'tin' => ucwords($user->CoTIN),
-    //                 'date_of_birth' => $user->CoBirthDate,
-    //                 'contact_number' => ucwords($user->CoNumber)
-    //             ]);
-    //         }
-
-    //         if ($user->AtFirstName && $user->AtLastName) {
-    //             Attorney::withoutGlobalScope('default_branch')
-    //             ->updateOrCreate([
-    //                 'first_name' => ucwords($user->AtFirstName),
-    //                 'last_name' => ucwords($user->AtLastName)
-    //             ],[
-    //                 'middle_name' => ucwords($user->AtMiddleName),
-    //                 'suffix' => ucwords($user->AtSuffix),
-    //                 'gender' => ucwords($user->AtGender),
-    //                 'tin' => ucwords($user->AtTIN),
-    //                 'date_of_birth' => $user->AtBirthDate,
-    //                 'contact_number' => ucwords($user->AtNumber)
-    //             ]);
-    //         }
-            
-    //         if ($user->Position) {
-    //             $user_data->employmentStatus()->withoutGlobalScope('default_branch')
-    //             ->updateOrCreate([
-    //                 'employment' => $user->Employment,
-    //                 'company_name' => $user->CompanyName,
-    //                 'location_of_work' => $user->CompanyLocation,
-    //                 'type_of_work' => $user->EmploymentType,
-    //                 'date_employed' => $user->DateEmployed,
-    //                 'profession' => $user->Profession,
-    //                 'position' => $user->Position,
-    //             ]);
-    //         }
-            
-    //     }
-
-        
-
-
         $slug = SlugService::createSlug(User::class, 'slug', 'Super Admin');
         $users =  [
             [
@@ -223,10 +31,54 @@ class UserSeeder extends Seeder
                 'email_verified_at' => now(),
                 'slug' => $slug,
                 'password' => Hash::make('secret123'), // password
+                'first_name' => 'Admin',
+                'last_name' => 'Admin',
+                'middle_name' => 'Admin',
+            ],
+            [
+                'role_id' => 6,
+                'branch_id' => 1,
+                'email' => 'honeybeb@admin.com',
+                'email_verified_at' => now(),
+                'slug' => SlugService::createSlug(User::class, 'slug', 'Honeybeb Bookkeeper'),
+                'password' => Hash::make('secret123'), // password
+                'first_name' => 'Honey Beb',
+                'last_name' => 'Admin',
+                'middle_name' => 'Admin',
+            ],
+            [
+                'role_id' => 6,
+                'branch_id' => 1,
+                'email' => 'rose@admin.com',
+                'email_verified_at' => now(),
+                'slug' => SlugService::createSlug(User::class, 'slug', 'Rose Bookkeeper'),
+                'password' => Hash::make('secret123'), // password
+                'first_name' => 'Rose',
+                'last_name' => 'Admin',
+                'middle_name' => 'Admin',
             ],
         ];
 
-        User::insert($users);
+        foreach ($users as $user) {
+            $admin = User::create([
+                'role_id' => $user['role_id'],
+                'branch_id' => $user['branch_id'],
+                'email' => $user['email'],
+                'email_verified_at' => now(),
+                'slug' => $user['slug'],
+                'password' => $user['password']
+            ]);
+
+            $admin->profile()->withoutGlobalScope('default_branch')
+            ->updateOrCreate([
+                'first_name' => ucwords(trim($user['first_name'])),
+                'last_name' => ucwords(trim($user['last_name'])),
+                'middle_name' => ($user['middle_name']) ? ucwords(trim($user['middle_name'])) : null
+            ], [
+                'photo' => null
+            ]);
+        }
+        
 
         $users = DB::connection('mysql2')->select("
         SELECT DISTINCT
